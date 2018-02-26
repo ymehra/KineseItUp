e07445aac8ad4f7398745104c8debc759fe51e47
Yash Mehra <ymehra@calpoly.edu>
2018-02-26 15:43:56 -0800

    python files

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+
+def load_Data(user, filename):
+   data = json.load(open('dir.json'))
+   dir = data[user] + filename
+   data = pd.read_csv(dir + '')
+   return data
+
+def main():
+   data = load_Data('Yash', '')
+   data.head()
+   
+main()
