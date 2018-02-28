b7c2c2f78662518bf6fe3203678adb033fd6bbd6
Yash Mehra <ymehra@calpoly.edu>
2018-02-28 14:06:37 -0800

    added some functions to test.py

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import json
 def load_Data(user, filename):
    data = json.load(open('dir.json'))
    dir = data[user] + filename
-   data = pd.read_csv(dir + '')
+   data = pd.read_csv(dir)
    return data
 
 def main():
    data = load_Data('Yash', '')
    data.head()
-   
+
 main()
