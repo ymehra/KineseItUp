8301a0fa01bc171fcee20b7ef92f1ed5d6c3c8d6
Yash Mehra <ymehra@calpoly.edu>
2018-05-17 15:41:18 -0700

    changed CNN.py so that it can take parameters and run hyper parameter tuning

--- CNN.py
+++ CNN.py
@@ -3,105 +3,117 @@ import numpy as np
 import script as sc
 import tensorflow as tf
 
-# data = pd.read_csv(r"C:\Users\yashm\Google Drive\Data Capstone_\Project Folder\PreWideData\cnn_wide.csv")
-data = pd.read_csv(sc.load_Data("AGG-Yash", "cnn_wide.csv"))
+def run_CNN(features, labels, K, epochs, learning_rate, window1, stride1, window2, stride2, outshape):
+   graph = tf.Graph()
+   with graph.as_default():
+      train_features = tf.placeholder(tf.float32, shape=(None, 240))
+      x = tf.reshape(train_features, [-1, 80, 3])
+      
+   with tf.Session(graph=graph) as sess:
+      tf.global_variables_initializer().run()
+      print(sess.run(x, feed_dict={train_features: features}))
 
-labels = data['activity']
-K = len(data['activity'].unique())
-features = data[data.columns[1:-2]]
+   num_epochs = epochs
 
-print(features.shape, len(labels), K)
+   graph = tf.Graph()
+   with graph.as_default():
+      # Placeholders for the actual data.
+      train_features = tf.placeholder(tf.float32, shape=(None, 240))
+      train_labels = tf.placeholder(tf.int32, shape=(None, ))
+      
+      # Reshaping the data
+      x = tf.reshape(train_features, [-1, 80, 3])
+      y = tf.one_hot(
+         train_labels,
+         depth=K
+      )
+      
+      # Convolution 1
+      conv1 = tf.layers.conv1d(inputs=x, 
+                              filters=8, 
+                              kernel_size=8, 
+                              padding="same", 
+                              data_format="channels_last",
+                              activation=tf.nn.relu)
+      # Max Pooling 1 (reduces samples from 80 --> 31)
+      pool1 = tf.layers.max_pooling1d(inputs=conv1,
+                                       pool_size=window1, 
+                                       strides=stride1,
+                                       data_format="channels_last")
+      
+      # Convolution 2
+      conv2 = tf.layers.conv1d(inputs=pool1, 
+                              filters=16, 
+                              kernel_size=8, 
+                              padding="same", 
+                              data_format="channels_last",
+                              activation=tf.nn.relu)
+      # Max Pooling 2 (reduces samples from 39 --> 17)
+      pool2 = tf.layers.max_pooling1d(inputs=conv2,
+                                       pool_size=window2, 
+                                       strides=stride2,
+                                       data_format="channels_last")
+      
+      # Flatten the Pooled Data
+      pool2_flat = tf.reshape(pool2, [-1, outshape * 16])
+      
+      # Dense Layer (try adding dropout?)
+      dense = tf.layers.dense(inputs=pool2_flat, units=128, activation=tf.nn.relu)
+      
+      # Logits Layer
+      logits = tf.layers.dense(inputs=dense, units=K)
+      
+      # Define loss function and optimizer
+      loss = tf.reduce_mean(
+         tf.nn.softmax_cross_entropy_with_logits(
+               logits=logits, 
+               labels=y
+         )
+      )
+      optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+      
+      # Define accuracy (so that it can be calculated and printed)
+      accuracy = tf.reduce_mean(
+         tf.cast(
+               tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1)),
+               tf.float32
+         )
+      )
+      
+   with tf.Session(graph=graph) as sess:
+      tf.global_variables_initializer().run()
+      i = 0
+      prev = 0
+      for _ in range(num_epochs):
+         feed_dict = {
+               train_features: features,
+               train_labels: labels
+         }
+         _, _, acc = sess.run(
+               [optimizer, loss, accuracy], 
+               feed_dict=feed_dict)
+         print("Epoch: ", i, " Acc: ", acc*100, " Change: ", (acc-prev)*100)
+         i = i + 1
+         prev = acc
 
+def main():
+   # data = pd.read_csv(r"C:\Users\yashm\Google Drive\Data Capstone_\Project Folder\PreWideData\cnn_wide.csv")
+   data = pd.read_csv(sc.load_Data("AGG-Yash", "cnn_wide.csv"))
 
-graph = tf.Graph()
-with graph.as_default():
-    train_features = tf.placeholder(tf.float32, shape=(None, 240))
-    x = tf.reshape(train_features, [-1, 80, 3])
-    
-with tf.Session(graph=graph) as sess:
-    tf.global_variables_initializer().run()
-    print(sess.run(x, feed_dict={train_features: features}))
+   labels = data['activity']
+   K = len(data['activity'].unique())
+   features = data[data.columns[1:-2]]
 
-num_epochs = 200
-learning_rate = 0.08
+   print(features.shape, len(labels), K)
 
-graph = tf.Graph()
-with graph.as_default():
-    # Placeholders for the actual data.
-    train_features = tf.placeholder(tf.float32, shape=(None, 240))
-    train_labels = tf.placeholder(tf.int32, shape=(None, ))
-    
-    # Reshaping the data
-    x = tf.reshape(train_features, [-1, 80, 3])
-    y = tf.one_hot(
-        train_labels,
-        depth=K
-    )
-    
-    # Convolution 1
-    conv1 = tf.layers.conv1d(inputs=x, 
-                             filters=8, 
-                             kernel_size=8, 
-                             padding="same", 
-                             data_format="channels_last",
-                             activation=tf.nn.relu)
-    # Max Pooling 1 (reduces samples from 80 --> 31)
-    pool1 = tf.layers.max_pooling1d(inputs=conv1,
-                                    pool_size=40, 
-                                    strides=2,
-                                    data_format="channels_last")
-    
-    # Convolution 2
-    conv2 = tf.layers.conv1d(inputs=pool1, 
-                             filters=16, 
-                             kernel_size=8, 
-                             padding="same", 
-                             data_format="channels_last",
-                             activation=tf.nn.relu)
-    # Max Pooling 2 (reduces samples from 39 --> 17)
-    pool2 = tf.layers.max_pooling1d(inputs=conv2,
-                                    pool_size=3, 
-                                    strides=2,
-                                    data_format="channels_last")
-    
-    # Flatten the Pooled Data
-    pool2_flat = tf.reshape(pool2, [-1, 10 * 16])
-    
-    # Dense Layer (try adding dropout?)
-    dense = tf.layers.dense(inputs=pool2_flat, units=128, activation=tf.nn.relu)
-    
-    # Logits Layer
-    logits = tf.layers.dense(inputs=dense, units=K)
-    
-    # Define loss function and optimizer
-    loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(
-            logits=logits, 
-            labels=y
-        )
-    )
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-    
-    # Define accuracy (so that it can be calculated and printed)
-    accuracy = tf.reduce_mean(
-        tf.cast(
-            tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1)),
-            tf.float32
-        )
-    )
-    
-with tf.Session(graph=graph) as sess:
-    tf.global_variables_initializer().run()
-    i = 0
-    prev = 0
-    for _ in range(num_epochs):
-        feed_dict = {
-            train_features: features,
-            train_labels: labels
-        }
-        _, _, acc = sess.run(
-            [optimizer, loss, accuracy], 
-            feed_dict=feed_dict)
-        print("Epoch: ", i, " Acc: ", acc*100, " Change: ", (acc-prev)*100)
-        i = i + 1
-        prev = acc
+   epochs = 200
+   learning_rate = 0.01
+   window1 = 40
+   stride1 = 2
+   window2 = 3
+   stride2 = 2
+   outshape = 10
+
+   run_CNN(features, labels, K, epochs, learning_rate, window1, stride1, window2, stride2, outshape)
+
+main()
