b8864ad93b816b064aa83454650d14df6b93a809
Hans Schumann <hansschumann24@gmail.com>
2018-05-31 12:47:08 -0700

    file to run all models (for cleaning)

---
+++ run_models.py
@@ -0,0 +1,20 @@
+## Authors: Yash Mehra, Gus Moir, Andrew Rose, Hans Schumann
+## Version: June 2018
+## 
+## This file will be run from command line to create a user-specified model
+## 
+
+# import the necessary files
+import sys
+import pandas as pd
+import numpy as np
+import json
+import sklearn
+# from sklearn.neural_network import MLPClassifier
+import script as sc
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import f1_score
+
+# >>> py run___ user model_want data_file pickle_output_name 
+
+# helper_functions
