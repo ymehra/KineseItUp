d1ad63c77aa68dc121afc65eee72f535225d2258
Hans Schumann <hansschumann24@gmail.com>
2018-06-09 13:10:18 -0700

    Finally done and ready for turn in.  Cross Validation stuff is there

--- final_code/run_models.py
+++ final_code/run_models.py
@@ -20,6 +20,7 @@ import sklearn.ensemble
 import sklearn.neural_network
 import sklearn.neighbors
 import pickle
+import math
 import script as sc
 
 # helper_functions
@@ -32,9 +33,7 @@ def save_pkl(classifier, filename):
 ## and return the fitted model
 def svm(trainX, trainY):
     clf = sklearn.svm.SVC(max_iter = 10)
-    print("clf made")
     clf.fit(trainX, trainY)
-    print("fit complete")
     return clf
 
 def neural_net(trainX, trainY):
@@ -59,120 +58,215 @@ def gradient_boost(trainX, trainY):
     return grad_boost
 
 def main(argv):
-   input_file = "Not found"
-   output_file = "Not found"
-   user = "Not found"
-   model = "Not found"
+  input_file = "Not found"
+  output_file = "Not found"
+  user = "Not found"
+  model = "Not found"
+  cross_val = "all"
 
-   try:
-      opts, args = getopt.getopt(argv,"hi:m:u:o:",["ifile=","mfile=","ufile=", "ofile="])
-   except getopt.GetoptError:
-      print ('run_models.py -i <inputfile> -m <model name> -u <user> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('run_models.py -i <inputfile> -m <model name> -u <user> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         input_file = arg
-      elif opt in ("-m", "--kvalue"):
-         model = arg
-      elif opt in ("-u", "--kvalue"):
-         user = arg
-      elif opt in ("-o", "--kvalue"):
-         output_file = arg
+  try:
+    opts, args = getopt.getopt(argv,"hi:m:u:o:c:",["ifile=","mfile=","ufile=", "ofile=", "cfile"])
+  except getopt.GetoptError:
+    print ('run_models.py -i <inputfile> -m <model name> -u <user> -o <outputfile> -c <crossvalmethod>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print ('run_models.py -i <inputfile> -m <model name> -u <user> -o <outputfile> -c <crossvalmethod>')
+      sys.exit()
+    elif opt in ("-i", "--ifile"):
+      input_file = arg
+    elif opt in ("-m", "--kvalue"):
+      model = arg
+    elif opt in ("-u", "--kvalue"):
+      user = arg
+    elif opt in ("-o", "--kvalue"):
+      output_file = arg
+    elif opt in ("-c", "--kvalue"):
+      cross_val = arg
 
-   print ('input file name given :', input_file)
-   print ('model given :', model)
-   print ('user given :', user)
-   print ('output file name given :', output_file)
+  print ('input file name given :', input_file)
+  print ('model given :', model)
+  print ('user given :', user)
+  print ('output file name given :', output_file)
+  print ('cross validation technique: ', cross_val)
 
-   model = model.lower()
+  model = model.lower()
+  cross_val = cross_val.lower()
    
-   ## cross validation by person (leave-one-out)
-   data = pd.DataFrame(sc.get_complete(user,input_file))
-   data = sc.get_lags(data, 1)
-   data = data.drop(['Unnamed: 0', 'Unnamed: 0.1', 'start.time', 'index', 'activity'], axis=1)
-   print(data.head())
-   data_with_predictions = pd.DataFrame()
+  ## cross validation by person (leave-one-out)
+  data = pd.DataFrame(sc.get_complete(user,input_file))
+  data = sc.get_lags(data, 1)
+  data = data.drop(['Unnamed: 0', 'Unnamed: 0.1', 'start.time', 'index', 'activity'], axis=1)
+  data_with_predictions = pd.DataFrame()
 
-   ## define variables
-   #x_vars = ['mean.vm','sd.vm','mean.ang','sd.ang','p625','dfreq','ratio.df']
-   y_var = 'coding'
+  ## define y variable
+  y_var = 'coding'
 
-   for person in np.unique(data['type']):
-      print (person)
+  ## perform the cross validation (depending on user input -- thx Alex...)
+  if cross_val == "default":
+    pass
+
+  elif cross_val == "leave_one_out":
+    for person in np.unique(data['type']):
       ## split training and testing by one person
       train = data[data['type'] != person].copy(deep=True)
       test = data[data['type'] == person].copy(deep=True)
 
-      #print(train.head)
-      
       ## subset x and y variables
       train_x = train.drop(['type', 'coding'], axis=1)
       train_y = train[y_var]
       test_x = test.drop(['type', 'coding'], axis=1)
       test_y = test[y_var]
 
-      print(train_x.head())
-
       classifier = None
-      print ("here")
 
       if model == "gradient_boosting":
-         classifier = gradient_boost(train_x,train_y)
+        classifier = gradient_boost(train_x,train_y)
       elif model == "svm":
-         print ("inside")
-         classifier = svm(train_x,train_y)
-         print("Finished SVM class")
+        train_x = sklearn.preprocessing.scale(train_x)
+        classifier = svm(train_x,train_y)
       elif model == "knn":
-         classifier = knn(train_x,train_y)
+        classifier = knn(train_x,train_y)
       elif model == "rf":
-         classifier = random_forest(train_x,train_y)
+        classifier = random_forest(train_x,train_y)
       elif model == "neural_network":
-         classifier = neural_net(train_x,train_y)
+        classifier = neural_net(train_x,train_y)
       else:
-         print ("why?")
-         exit(1)
-      print("out of condtional")
+        exit(1)
+
       ## use classifier to predict
+      if model == "svm":
+        test_x = sklearn.preprocessing.scale(test_x)
       pred = classifier.predict(test_x)
       test['predicted'] = pred
-      print ("outside")
+
       ## print the accuracy of current person
       corr = test['predicted'] == test['coding']
       print (str(person + " accuracy = "),sum(corr) / len(corr))
 
       ## append the data set with predictions for that person
       data_with_predictions = data_with_predictions.append(test)
-      print ("bottom")
 
-   ## train classifier on all the data
-   if model == "gradient_boosting":
-      classifier = gradient_boost(data.drop(['coding', 'type'], axis=1), data[y_var])
-   elif model == "svm":
-      print("inside")
-      classifier = svm(data.drop(['coding', 'type'], axis=1), data[y_var])
-      print("Finished SVM class")
-   elif model == "knn":
-      classifier = knn(data.drop(['coding', 'type'], axis=1), data[y_var])
-   elif model == "rf":
-      classifier = random_forest(data.drop(['coding', 'type'], axis=1), data[y_var])
-   elif model == "neural_network":
-      classifier = neural_net(data.drop(['coding', 'type'], axis=1), data[y_var])
-   else:
-      print("why?")
-      exit(1)
+  ## k-fold cross validation
+  elif cross_val.isdigit():
+    people = np.unique(data['type'])
+    np.random.shuffle(people)
+    k = int(cross_val)
+    how_many = (len(people) / k)
+    
+    for i in range(k):
+      test_group = people[int(how_many * i):int(how_many * (i+1))]
+      train = data[~data['type'].isin(test_group)].copy(deep=True)
+      test = data[data['type'].isin(test_group)].copy(deep=True)
 
-   ## output the classifer and data_set with predictions
-   save_pkl(classifier, output_file)
+      ## subset x and y variables
+      train_x = train.drop(['type', 'coding'], axis=1)
+      train_y = train[y_var]
+      test_x = test.drop(['type', 'coding'], axis=1)
+      test_y = test[y_var]
 
-   ## write the data to a csv
-   data_with_predictions.to_csv(str(model+'_new_')+input_file,index = False)
-   ## print the cross-val score
-   correct = data_with_predictions['coding'] == data_with_predictions['predicted']
-   print ("Overall Model Accuracy = ",sum(correct) / len(correct))
+      classifier = None
+
+      if model == "gradient_boosting":
+        classifier = gradient_boost(train_x,train_y)
+      elif model == "svm":
+        train_x = sklearn.preprocessing.scale(train_x)
+        classifier = svm(train_x,train_y)
+      elif model == "knn":
+        classifier = knn(train_x,train_y)
+      elif model == "rf":
+        classifier = random_forest(train_x,train_y)
+      elif model == "neural_network":
+        classifier = neural_net(train_x,train_y)
+      else:
+        exit(1)
+
+      ## use classifier to predict
+      if model == "svm":
+        test_x = sklearn.preprocessing.scale(test_x)
+      pred = classifier.predict(test_x)
+      test['predicted'] = pred
+
+      ## print the accuracy of current person
+      corr = test['predicted'] == test['coding']
+      print ("People: ",test_group)
+      print (str("Accuracy = "),sum(corr) / len(corr))
+      print ()
+
+      ## append the data set with predictions for that person
+      data_with_predictions = data_with_predictions.append(test)
+
+  elif cross_val == "domain":
+    all_domains = ['A','W','E','L','H']
+    for d in all_domains:
+      ## get the ones from the domain
+      subset = [v for i, v in enumerate(data['type']) if d in v]
+      small_data = data[data['type'].isin(subset)]
+      ## go through each person in the subgroup (domain)
+      for person in np.unique(subset):
+        ## split training and testing by one person
+        train = small_data[small_data['type'] != person].copy(deep=True)
+        test = small_data[small_data['type'] == person].copy(deep=True)
+
+        ## subset x and y variables
+        train_x = train.drop(['type', 'coding'], axis=1)
+        train_y = train[y_var]
+        test_x = test.drop(['type', 'coding'], axis=1)
+        test_y = test[y_var]
+
+        classifier = None
+
+        if model == "gradient_boosting":
+          classifier = gradient_boost(train_x,train_y)
+        elif model == "svm":
+          train_x = sklearn.preprocessing.scale(train_x)
+          classifier = svm(train_x,train_y)
+        elif model == "knn":
+          classifier = knn(train_x,train_y)
+        elif model == "rf":
+          classifier = random_forest(train_x,train_y)
+        elif model == "neural_network":
+          classifier = neural_net(train_x,train_y)
+        else:
+          exit(1)
+
+        ## use classifier to predict
+        if model == "svm":
+          test_x = sklearn.preprocessing.scale(test_x)
+        pred = classifier.predict(test_x)
+        test['predicted'] = pred
+
+        ## print the accuracy of current person
+        corr = test['predicted'] == test['coding']
+        print (str(person + " accuracy = "),sum(corr) / len(corr))
+
+        ## append the data set with predictions for that person
+        data_with_predictions = data_with_predictions.append(test)
+
+
+  ## train classifier on all the data
+  if model == "gradient_boosting":
+    classifier = gradient_boost(data.drop(['coding', 'type'], axis=1), data[y_var])
+  elif model == "svm":
+    classifier = svm(data.drop(['coding', 'type'], axis=1), data[y_var])
+  elif model == "knn":
+    classifier = knn(data.drop(['coding', 'type'], axis=1), data[y_var])
+  elif model == "rf":
+    classifier = random_forest(data.drop(['coding', 'type'], axis=1), data[y_var])
+  elif model == "neural_network":
+    classifier = neural_net(data.drop(['coding', 'type'], axis=1), data[y_var])
+  else:
+    exit(1)
+
+  ## output the classifer and data_set with predictions
+  save_pkl(classifier, output_file)
+
+  ## write the data to a csv
+  data_with_predictions.to_csv(str(model+'_new_')+input_file,index = False)
+  ## print the cross-val score
+  correct = data_with_predictions['coding'] == data_with_predictions['predicted']
+  print ("Overall Model Accuracy = ",sum(correct) / len(correct))
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+  main(sys.argv[1:])
 
